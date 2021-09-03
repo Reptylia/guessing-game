a26f392dabaf24371324d35d2f4392611c1b6364
Reptylia <repty.alliance@gmail.com>
2021-09-03 20:47:03 +0300

    feat: finish

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,32 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+        
+    }
     setRange(min, max) {
+    
+    this.min1 = min;
+    this.max2 = max;
+    this.raznica = 0;
+
+    this.poppy = Math.round(max / 2);
 
     }
 
     guess() {
-
+    return this.poppy;
     }
 
     lower() {
-
+    this.max2 = this.poppy;
+    this.raznica = ((this.max2 - this.min1) / 2);
+    this.poppy = Math.round(this.poppy - this.raznica);
     }
 
     greater() {
-
+    this.min1 = this.poppy;
+    // this.raznica = Math.round(this.raznica / 2);
+    this.raznica = ((this.max2 - this.min1) / 2);
+    this.poppy = Math.round(this.min1 + this.raznica);
     }
 }
 
